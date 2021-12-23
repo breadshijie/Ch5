d4c5b0089c0e0703bd3e574dad94702dffab8c99
breadshijie <breadshijie@gmail.com>
2021-12-23 16:41:33 +0800

    Ch5

---
+++ Ch5_p14/src/pb/Ch5_p14.java
@@ -0,0 +1,13 @@
+package pb;
+
+import pc.Car;
+
+public class Ch5_p14
+{
+	public static void main(String[] args) 
+	{
+		Car car1;
+		car1 = new Car();
+		car1.show();
+	}
+}
---
+++ Ch5_p30/src/Ch5_p30.java
@@ -0,0 +1,56 @@
+
+public class Ch5_p30 
+{
+	public static void main(String[] args) 
+	{
+		Car car1;
+		car1 = new Car();
+		
+		try 
+		{
+			car1.setCar(1234,-10);
+		}
+		catch(CarException e)
+		{
+			System.out.println("跳出"+e+"了");
+		}
+		car1.show();
+	}
+}
+class Car
+{
+	protected int num;
+	protected double gas;
+	
+	public Car()
+	{
+		num=0;
+		gas=0;
+		System.out.println("生產了車子");
+	}
+	
+	public void setCar(int n,double g) throws CarException
+	{
+		if(g<0)
+		{
+			CarException e = new CarException();
+			throw e;
+		}
+		else
+		{
+		num=n;
+		gas=g;
+		System.out.println("將車號設為"+num+"，汽油量設為"+gas);
+		}
+	}
+	
+	public void show()
+	{
+		System.out.println("車號是"+num);
+		System.out.println("汽油量是"+gas);
+	}
+}
+class CarException extends Exception
+{
+	
+}
